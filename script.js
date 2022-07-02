d9c8324230bf838effe5758499a4c84bc950a31f
Adnan-001 <107484208+Adnan-001@users.noreply.github.com>
2022-07-02 16:49:50 +0500

    Add computerPlay() to script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("testing");
+
+function computerPlay() {
+    
+    let randNo = Math.floor(Math.random()*3);
+    
+    if (randNo === 0) {
+        return 'Rock'; 
+    }
+    else if (randNo === 1)
+    {
+        return 'Paper';
+    }
+    else {
+        return 'Scissors';
+    }
+
+}
+
+console.log(computerPlay());
+
+
