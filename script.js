a5f331044bc62deb960e3838fb1c73220f7debab
Adnan-001 <107484208+Adnan-001@users.noreply.github.com>
2022-07-02 17:13:05 +0500

    Implement playRound() in script.js

--- script.js
+++ script.js
@@ -16,6 +16,33 @@ function computerPlay() {
 
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, compSelection) {
+    let result;
+
+    if ( (playerSelection.toUpperCase() === "ROCK") && compSelection === "Scissors"
+        ||  (playerSelection.toUpperCase() === "PAPER") && compSelection === "Rock"  
+        ||  (playerSelection.toUpperCase() === "SCISSORS") && compSelection === "Paper" ) 
+    {
+
+        result =   `You Won! ${playerSelection} beats ${compSelection}!`;
+    }
+    else if ( (playerSelection.toUpperCase() === "ROCK") && compSelection === "Paper"
+        ||  (playerSelection.toUpperCase() === "PAPER") && compSelection === "Scissors"  
+        ||  (playerSelection.toUpperCase() === "SCISSORS") && compSelection === "Rock" ) 
+    {
+        result =   `You Lost! ${compSelection} beats ${playerSelection}!`;
+    }
+    else
+    {
+        result = `It is a tie! you both went ${compSelection}!`;
+    }
+    return result;
+}
+
+
+const computerSelection = computerPlay();
+console.log(computerSelection);
+const playerSelection = "pApEr";
+console.log(playRound(playerSelection, computerSelection));
 
 
